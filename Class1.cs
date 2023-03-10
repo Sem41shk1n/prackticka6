34336a46b3a57d4e6299852bae8b2960406d9e21
sem41shk1n <ilyxabezyxa@mail.ru>
2023-03-10 23:19:30 +0300

    practika6

---
+++ Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace текстовый_конвентер
+{
+    public class Class1
+    {
+        public string name;
+        public int age;
+        public Class1() { }
+
+        public Class1(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+    }
+}
